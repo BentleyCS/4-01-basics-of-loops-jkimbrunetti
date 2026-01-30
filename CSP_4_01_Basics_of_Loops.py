de94e22772d86e3a12885ff6eeca7425f6aa9a6d
jkimbrunetti <jkim-brunetti27@bentleyschool.org>
2026-01-29 21:22:48 -0800

    Add files via upload

--- CSP_4_01_Basics_of_Loops.py
+++ CSP_4_01_Basics_of_Loops.py
@@ -1,88 +1,97 @@
 #All questions must use a loop for full points.
+import random
 
 def oddNumbers(n:int) ->str:
-    """
-    Print out all odd numbers from 1 to n(inclusive) in a single string seperated by spaces.
-    example oddNumbers(5) -> "1 3 5"
-    example oddNumbers(8) -> "1 3 5 7"
-    example oddNumbers(-8) -> ""
-    """
-
+    odd = ""
+    for i in range(1, n + 1, 1):
+        if i < n:
+            if i % 2 != 0:
+                odd += str(i)
+            elif i % 2 == 0:
+                odd += " "
+        if i == n and i % 2 == 1:
+            odd += str(i)
+    print(odd)
+    return odd
 
 def backwards(n)-> int:
-    """
-    modify the below function such that it prints out all the numbers from n to 1
-    inclusive starting at n and counting down to 1
-    example backwards(5) -> "5 4 3 2 1"
-    example backwards(8) -> "8 7 6 5 4 3 2 1"
-    example backwards(-2) -> ""
-    """
-
-
+    bw = ""
+    for i in range(n, 0, -1):
+        if i > 1:
+            bw += str(i) + " "
+        elif i == 1:
+            bw += "1"
+    print(bw)
+    return bw
 
 def randomRepeating():
-    """
-    Print out a random number from 1-10 until you get a 10. Then print out how many
-    times it took to roll a 10
-    NOTE: Given randomness no test for this question
-    :return:
-    """
+    newNum = random.randint(1,10)
     tries = 0
-    print(f"it took {tries} tries to get a 10")
+    while newNum in range(1,9):
+        tries +=1
+        newNum = random.randint(1,10)
+    return f"it took {tries} tries to get a 10"
+
 def randomRange(n):
-    """
-    Generate a random number from 1 to 100 n number of times. Then after that is
-    done print out what the highest number and the lowers number was from the rolled numbers.
-    NOTE: Given randomness no test for this question
-    :param n:
-    :return:
-    """
+    list = []
+    for i in range(0, n, 1):
+        newNum = random.randint(1, 100)
+        n+=1
+        list.append(newNum)
+    print(list)
+    list.sort()
+    low = list[0]
+    high = list[len(list) - 1]
+    print(f"Low = {low}, High = {high}")
+    return low, high
+
 def reverse(word:str)->str:
-    """
-    Takes in a string as an argument and return the given string in reverse.
-    example reverse("cat") -> "tac"
-    example reverse("Hello") -> "olleH"
-    """
+    reverseWord = ""
+    for i in range(len(word) - 1, -1, -1):
+        reverseWord += word[i]
+    return reverseWord
 
 def fizzBuzzContinuous(n):
-    """
-    Modify the function such that it does the fizzbuzz operation on all numbers
-    from 1 to n(inclusive).
-    Fizz buzz is defined as
-    if the number is divisble by 3 print fizz
-    if the number is divisible by 5 print buzz
-    if the number is divisible by both 3 and 5 print fizzbuzz
-    if none of the above apply print the number.
-
-    As with above questions add each anseer to a string and return the final string.
-    :param n:
-    :return:
-    """
+    ans = ""
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            ans += "fizzbuzz"
+        elif i % 3 == 0:
+            ans += "fizz"
+        elif i % 5 == 0:
+            ans += "buzz"
+        else:
+            ans += str(i)
+        if i != n:
+            ans += " "
+    return ans
 
 def collatz(n):
-    """
-    Modify this function such that it mimics the collatz conjecture starting at n
-    and prints out each number.
-    The collatz conjecture is that if n is an even number divide it by 2. if n is
-    an odd number multiply it by 3 and add 1.
-    Repeat this process until n == 1.
-    :param n:
-    :return:
-    """
+    ans = ""
+    while n != 1:
+        ans += str(n) + " "
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n * 3 + 1
+    ans += "1"
+    print(ans)
+    return ans
 
 
 def fibonacci(n):
-    """
-    for the given argument n print out the first n numbers of the fibonacci
-    sequence in a single string sperated by spaces.
-    The fibonacci sequence is defined as a sequence that starts with 0 then 1 as
-    the first two numbers. Every subsequent number is the prior two numbers added together.
-    Example fibonacci(6) -> "0 1 1 2 3 5"
-    Example fibonacci(10) -> "0 1 1 2 3 5 8 13 21 34"
-    Example fibonacci(1) -> "0"
-    :param n:
-    :return:
-    """
-
+    if n <= 0:
+        return ""
+    elif n == 1:
+        return "0"
+    a = 0
+    b = 1
+    ans = "0 1"
+    for i in range(2, n):
+        c = a + b
+        ans += " " + str(c)
+        a = b
+        b = c
+    return ans
 
 print(fibonacci(300))
